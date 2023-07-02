1e8cd4114936f5b9eceecfeb84c6c4234409a3fa
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-02 00:39:48 -0300

    links

--- costs/src/components/layout/Footer.js
+++ costs/src/components/layout/Footer.js
@@ -6,9 +6,9 @@ function Footer(){
     return(
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
-                <li><FaGithub/></li>
-                <li><FaInstagram/></li>
-                <li><FaLinkedin/></li>
+                <li><a href="https://github.com/Pedroanjoss"><FaGithub/></a></li>
+                <li><a href="https://www.instagram.com/pedrooanjos/"><FaInstagram/></a></li>
+                <li><a href="https://www.linkedin.com/in/pedro-dos-anjos-b8659b19b/"><FaLinkedin/></a></li>
             </ul>
             <p className={styles.copy_right}>  <span>Costs</span> &copy; 2023</p>
         </footer>
