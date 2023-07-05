00943effe44eb17e65ba7131fcef835a7a7a0c89
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-04 23:36:42 -0300

    mensagem do sistema

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,5 +1,34 @@
+import {useLocation} from 'react-router-dom'
+
+import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from '../pages/LinkButton'
+
+import styles from './Projects.module.css'
+
 function Projects() {
-    return <h1>Projects</h1>
+
+   const location = useLocation() 
+   let message = ''
+   if (location.state) {
+    message = location.state.message
+   } 
+
+   
+
+    return (
+
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+            {message && <Message type="success" msg={message}/>}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
+        </div>
+    )
 }
 
 export default Projects
