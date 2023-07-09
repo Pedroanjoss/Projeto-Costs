d91423d34f040808936bd928c63882d47f77e4e7
Pedro dos Anjos <pedrodeanjos@gmail.com>
2023-07-08 22:03:43 -0300

    excluindo servicos

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -109,8 +109,31 @@ function Project() {
         setShowProjectForm(!showProjectForm)
     }
 
-    function removeService(){
+    function removeService(id,cost){
 
+        const serviceUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+
+        const projectUpdated = project
+
+        projectUpdated.services = serviceUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+    
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(projectUpdated)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(serviceUpdated)
+            setMessage('Serviço removido com secesso!')
+            setType('success')
+        })
+        .catch(err => console.log(err))
     }
 
     function toggleServiceForm(){
